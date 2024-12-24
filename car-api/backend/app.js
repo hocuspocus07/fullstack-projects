660120ce048c72fe4cd69965222905dc06504f20
hocuspocus07 <150130202+hocuspocus07@users.noreply.github.com>
2024-12-24 19:16:03 +0530

    added apikey exception for cronjob

--- car-api/backend/app.js
+++ car-api/backend/app.js
@@ -24,10 +24,15 @@ app.use(cookieParser());
 
 import rateLimit from 'express-rate-limit';
 
+const myApiKey=process.env.MY_CRON_API;
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
     message: 'Too many requests, please try again later',
+    skip: (req) => {
+        const apiKey = req.query.apiKey || req.header("apiKey");
+        return myApiKey.includes(apiKey);
+    },
 });
 
 app.use('/api/', apiLimiter);
